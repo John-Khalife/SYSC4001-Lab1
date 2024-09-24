fb0c07819e9944258e753c2d36a29efb1dd320e3
John Khalife <jpa.khalife@gmail.com>
2024-09-24 12:32:23 -0400

    changed main script

--- CPU.h
+++ CPU.h
@@ -9,59 +9,40 @@ class CPU {
         //Member variables
         int clock; //Number of clock cycles
         bool kernelMode; // Whether or not the CPU is currently in kernel mode.
-        int programCounter; // this is used to increment the counter
-    
+        int programCounter; // Used to increment the counter
+        int time; //used to keep track of time
     
     public:
 
+        /**
+        * Constructor of the CPU class.
+        */
         CPU() {
             clock = 0;
+            time = 0;
+            programCounter = 0;
             kernelMode = true;
         }
-
- 
-        /**
-        * This method is intended to calculate the info to display based on the read from the sensor
-        */
-        // int calculate(int sensorInput) {
-
-        // }
-
-        /**
-         * This method handles interrupts
-        */
-        // void handleInterrupt(int interruptNum) {
-
-        // }
 
         /**
          * This method changes the kernelMode.
         */
-        // void setKernelMode(bool mode) {
-        //     kernelMode = mode;
-        // }
-
+        void switchCPUMode();
 
         /**
          * This method fetches the kernelMode.
         */
-        // bool isKernelMode() {
-        //     return kernelMode;
-        // }
+        bool isKernelMode();
 
-
-    
         /**
          * Method used to write to the output file
         */
-        void writeToFile();
-
+        void writeExecutionStep(int duration, std::string eventType);
 
         /**
          * This method is intended to be used for a system call - it checks the input device.
-         * 
         */
-
+        void systemCall(int duration, int isrAddress);
 
         /**
          * This method is intended to be used to display information on the output device
--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,7 @@
 int main() {
     std::cout << "This, is a test, nothing more.";
     CPU cpu = CPU();
-    cpu.writeToFile();
+    cpu.writeExecutionStep(50,"Initialize variables");
+    cpu.writeExecutionStep(70,"This is a test of a second instrcution - does it append?");
     return 0;
 }
