830a42563a1eaa2a63e4944e7904c5247eb72f0c
John Khalife <jpa.khalife@gmail.com>
2024-10-03 12:50:49 -0400

    - Fixed the createTestTrace.cpp

---
+++ additionalFiles/evaluateOutput.cpp
@@ -0,0 +1,8 @@
+/**
+ * This file holds code responsible for ensuring that the output is correct
+ * @author John Khalife, Stavros Karamalis
+*/
+
+int main(int argc, char* argv[]) {
+
+}
--- test2.sh
+++ test2.sh
@@ -1,2 +1,2 @@
 g++  interrupts.cpp -I interrupts.hpp -o sim
-./sim trace2.txt execution2.txt
+./sim trace2.txt 
